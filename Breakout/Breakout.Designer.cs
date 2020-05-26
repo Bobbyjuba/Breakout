844837eed42b9b9ccd33e99c534eeb1e4d3ca5ea
Brian Roden <Bobbyjuba@gmail.com>
2020-05-25 23:12:57 -0500

    Began the workings of a restart prompt

--- Breakout/Breakout.Designer.cs
+++ Breakout/Breakout.Designer.cs
@@ -23,96 +23,97 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
-            this.picBall = new System.Windows.Forms.PictureBox();
-            this.picPaddle = new System.Windows.Forms.PictureBox();
-            this.gameTimer = new System.Windows.Forms.Timer(this.components);
-            this.labelScore = new System.Windows.Forms.Label();
-            this.labelScoreVal = new System.Windows.Forms.Label();
-            this.floor = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picPaddle)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.floor)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // picBall
-            // 
-            this.picBall.BackColor = System.Drawing.Color.IndianRed;
-            this.picBall.Location = new System.Drawing.Point(275, 275);
-            this.picBall.Name = "picBall";
-            this.picBall.Size = new System.Drawing.Size(15, 15);
-            this.picBall.TabIndex = 0;
-            this.picBall.TabStop = false;
-            // 
-            // picPaddle
-            // 
-            this.picPaddle.BackColor = System.Drawing.Color.IndianRed;
-            this.picPaddle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.picPaddle.Location = new System.Drawing.Point(215, 490);
-            this.picPaddle.Name = "picPaddle";
-            this.picPaddle.Size = new System.Drawing.Size(125, 15);
-            this.picPaddle.TabIndex = 1;
-            this.picPaddle.TabStop = false;
-            // 
-            // gameTimer
-            // 
-            this.gameTimer.Enabled = true;
-            this.gameTimer.Interval = 10;
-            this.gameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
-            // 
-            // labelScore
-            // 
-            this.labelScore.BackColor = System.Drawing.Color.White;
-            this.labelScore.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.labelScore.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelScore.Location = new System.Drawing.Point(220, 0);
-            this.labelScore.Name = "labelScore";
-            this.labelScore.Size = new System.Drawing.Size(100, 20);
-            this.labelScore.TabIndex = 2;
-            this.labelScore.Text = "Score:";
-            this.labelScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // labelScoreVal
-            // 
-            this.labelScoreVal.BackColor = System.Drawing.Color.White;
-            this.labelScoreVal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.labelScoreVal.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelScoreVal.Location = new System.Drawing.Point(320, 0);
-            this.labelScoreVal.Name = "labelScoreVal";
-            this.labelScoreVal.Size = new System.Drawing.Size(29, 20);
-            this.labelScoreVal.TabIndex = 3;
-            this.labelScoreVal.Text = "0";
-            this.labelScoreVal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // floor
-            // 
-            this.floor.BackColor = System.Drawing.Color.White;
-            this.floor.Cursor = System.Windows.Forms.Cursors.Default;
-            this.floor.Location = new System.Drawing.Point(-1, 553);
-            this.floor.Name = "floor";
-            this.floor.Size = new System.Drawing.Size(586, 10);
-            this.floor.TabIndex = 4;
-            this.floor.TabStop = false;
-            // 
-            // BreakoutForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(584, 561);
-            this.Controls.Add(this.floor);
-            this.Controls.Add(this.labelScoreVal);
-            this.Controls.Add(this.labelScore);
-            this.Controls.Add(this.picPaddle);
-            this.Controls.Add(this.picBall);
-            this.Name = "BreakoutForm";
-            this.Text = "Breakout";
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BreakoutForm_KeyDown);
-            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.BreakoutForm_MouseMove);
-            ((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picPaddle)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.floor)).EndInit();
-            this.ResumeLayout(false);
+			this.components = new System.ComponentModel.Container();
+			this.picBall = new System.Windows.Forms.PictureBox();
+			this.picPaddle = new System.Windows.Forms.PictureBox();
+			this.gameTimer = new System.Windows.Forms.Timer(this.components);
+			this.labelScore = new System.Windows.Forms.Label();
+			this.labelScoreVal = new System.Windows.Forms.Label();
+			this.floor = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picPaddle)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.floor)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// picBall
+			// 
+			this.picBall.BackColor = System.Drawing.Color.IndianRed;
+			this.picBall.Location = new System.Drawing.Point(275, 275);
+			this.picBall.Name = "picBall";
+			this.picBall.Size = new System.Drawing.Size(15, 15);
+			this.picBall.TabIndex = 0;
+			this.picBall.TabStop = false;
+			// 
+			// picPaddle
+			// 
+			this.picPaddle.BackColor = System.Drawing.Color.IndianRed;
+			this.picPaddle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picPaddle.Location = new System.Drawing.Point(215, 490);
+			this.picPaddle.Name = "picPaddle";
+			this.picPaddle.Size = new System.Drawing.Size(125, 15);
+			this.picPaddle.TabIndex = 1;
+			this.picPaddle.TabStop = false;
+			// 
+			// gameTimer
+			// 
+			this.gameTimer.Enabled = true;
+			this.gameTimer.Interval = 10;
+			this.gameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
+			// 
+			// labelScore
+			// 
+			this.labelScore.BackColor = System.Drawing.Color.White;
+			this.labelScore.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.labelScore.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelScore.Location = new System.Drawing.Point(220, 0);
+			this.labelScore.Name = "labelScore";
+			this.labelScore.Size = new System.Drawing.Size(100, 20);
+			this.labelScore.TabIndex = 2;
+			this.labelScore.Text = "Score:";
+			this.labelScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// labelScoreVal
+			// 
+			this.labelScoreVal.BackColor = System.Drawing.Color.White;
+			this.labelScoreVal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.labelScoreVal.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelScoreVal.Location = new System.Drawing.Point(320, 0);
+			this.labelScoreVal.Name = "labelScoreVal";
+			this.labelScoreVal.Size = new System.Drawing.Size(29, 20);
+			this.labelScoreVal.TabIndex = 3;
+			this.labelScoreVal.Text = "0";
+			this.labelScoreVal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// floor
+			// 
+			this.floor.BackColor = System.Drawing.Color.White;
+			this.floor.Cursor = System.Windows.Forms.Cursors.Default;
+			this.floor.Location = new System.Drawing.Point(-1, 553);
+			this.floor.Name = "floor";
+			this.floor.Size = new System.Drawing.Size(586, 10);
+			this.floor.TabIndex = 4;
+			this.floor.TabStop = false;
+			// 
+			// BreakoutForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(584, 561);
+			this.Controls.Add(this.floor);
+			this.Controls.Add(this.labelScoreVal);
+			this.Controls.Add(this.labelScore);
+			this.Controls.Add(this.picPaddle);
+			this.Controls.Add(this.picBall);
+			this.MaximizeBox = false;
+			this.Name = "BreakoutForm";
+			this.Text = "Breakout";
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BreakoutForm_KeyDown);
+			this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.BreakoutForm_MouseMove);
+			((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picPaddle)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.floor)).EndInit();
+			this.ResumeLayout(false);
 
         }
 
